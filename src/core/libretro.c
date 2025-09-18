2c514cd54a098dc3fc3fef246678e3e874f22f92
yuichietsu <osoba.udon.men@gmail.com>
2025-09-19 00:25:45 +0900

    refactor(core): remove unused audio/input code

--- src/core/libretro.c
+++ src/core/libretro.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-#include <math.h>
 
 #include <stdio.h>
 #if defined(_WIN32) && !defined(_XBOX)
@@ -21,9 +20,6 @@
 static uint8_t *frame_buf;
 static struct retro_log_callback logging;
 static retro_log_printf_t log_cb;
-static bool use_audio_cb;
-static float last_aspect;
-static float last_sample_rate;
 char retro_base_directory[4096];
 char retro_game_path[4096];
 char retro_core_path[4096];
@@ -67,7 +63,8 @@ unsigned retro_api_version(void)
 
 void retro_set_controller_port_device(unsigned port, unsigned device)
 {
-   log_cb(RETRO_LOG_INFO, "Plugging device %u into port %u.\n", device, port);
+   (void)port;
+   (void)device;
 }
 
 void retro_get_system_info(struct retro_system_info *info)
@@ -80,25 +77,17 @@ void retro_get_system_info(struct retro_system_info *info)
 }
 
 static retro_video_refresh_t video_cb;
-static retro_audio_sample_t audio_cb;
-static retro_audio_sample_batch_t audio_batch_cb;
-static retro_input_poll_t input_poll_cb;
-static retro_input_state_t input_state_cb;
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
 {
-   float aspect                = 0.0f;
-   float sampling_rate         = 30000.0f;
-
-
    info->geometry.base_width   = VIDEO_WIDTH;
    info->geometry.base_height  = VIDEO_HEIGHT;
    info->geometry.max_width    = VIDEO_WIDTH;
    info->geometry.max_height   = VIDEO_HEIGHT;
-   info->geometry.aspect_ratio = aspect;
+   info->geometry.aspect_ratio = 0.0f;
 
-   last_aspect                 = aspect;
-   last_sample_rate            = sampling_rate;
+   info->timing.fps            = 60.0;
+   info->timing.sample_rate    = 0.0;
 }
 
 void retro_set_environment(retro_environment_t cb)
@@ -110,15 +99,7 @@ void retro_set_environment(retro_environment_t cb)
    else
       log_cb = fallback_log;
 
-   static const struct retro_controller_description controllers[] = {
-      { "Nintendo DS", RETRO_DEVICE_SUBCLASS(RETRO_DEVICE_JOYPAD, 0) },
-   };
 
-   static const struct retro_controller_info ports[] = {
-      { controllers, 1 },
-      { NULL, 0 },
-   };
-   cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
 
    bool no_content = true;
    cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &no_content);
@@ -161,22 +142,22 @@ void init_core_options(void)
 
 void retro_set_audio_sample(retro_audio_sample_t cb)
 {
-   audio_cb = cb;
+   (void)cb;
 }
 
 void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb)
 {
-   audio_batch_cb = cb;
+   (void)cb;
 }
 
 void retro_set_input_poll(retro_input_poll_t cb)
 {
-   input_poll_cb = cb;
+   (void)cb;
 }
 
 void retro_set_input_state(retro_input_state_t cb)
 {
-   input_state_cb = cb;
+   (void)cb;
 }
 
 void retro_set_video_refresh(retro_video_refresh_t cb)
@@ -184,44 +165,18 @@ void retro_set_video_refresh(retro_video_refresh_t cb)
    video_cb = cb;
 }
 
-static unsigned phase;
-
 void retro_reset(void)
 {
 
 }
-
-static void update_input(void)
-{
-
-}
-
 
 static void check_variables(void)
 {
 
 }
 
-static void audio_callback(void)
-{
-   for (unsigned i = 0; i < 30000 / 60; i++, phase++)
-   {
-      int16_t val = 0x800 * sinf(2.0f * M_PI * phase * 300.0f / 30000.0f);
-      audio_cb(val, val);
-   }
-
-   phase %= 100;
-}
-
-static void audio_set_state(bool enable)
-{
-   (void)enable;
-}
-
 void retro_run(void)
 {
-   update_input();
-
 	video_cb(frame_buf, VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_WIDTH * sizeof(uint32_t));
 
    bool updated = false;
@@ -231,16 +186,6 @@ void retro_run(void)
 
 bool retro_load_game(const struct retro_game_info *info)
 {
-   struct retro_input_descriptor desc[] = {
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,  "Left" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,    "Up" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,  "Down" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT, "Right" },
-      { 0 },
-   };
-
-   environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
-
    enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
    if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
    {
@@ -251,8 +196,6 @@ bool retro_load_game(const struct retro_game_info *info)
 	if (info != NULL) {
 		snprintf(retro_game_path, sizeof(retro_game_path), "%s", info->path);
 	}
-   struct retro_audio_callback audio_cb = { audio_callback, audio_set_state };
-   use_audio_cb = environ_cb(RETRO_ENVIRONMENT_SET_AUDIO_CALLBACK, &audio_cb);
 
    check_variables();
 
