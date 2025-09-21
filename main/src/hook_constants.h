e1f55c26e18f9e3e4644a20ee2e360979a41b4bd
yuichietsu <osoba.udon.men@gmail.com>
2025-09-22 00:15:22 +0900

    Remove unused constants from hook_constants.h
    - Remove HOOK_PATH_SEPARATOR (unused across the project)
    - Remove MAX_ERROR_MSG_SIZE (unused across the project)
    
    These constants were defined but never referenced in any source files,
    following the project guideline to remove unused code with low reusability.

--- main/src/hook_constants.h
+++ main/src/hook_constants.h
@@ -1,13 +1,6 @@
 #ifndef HOOK_CONSTANTS_H
 #define HOOK_CONSTANTS_H
 
-/* Path separator */
-#ifdef _WIN32
-#define HOOK_PATH_SEPARATOR "\\"
-#else
-#define HOOK_PATH_SEPARATOR "/"
-#endif
-
 /* Video constants */
 #define VIDEO_WIDTH 640
 #define VIDEO_HEIGHT 480
@@ -17,6 +10,5 @@
 #define MAX_PATH_SIZE 4096
 #define MAX_COMMAND_SIZE 2048
 #define MAX_SCRIPT_PATH_SIZE 512
-#define MAX_ERROR_MSG_SIZE 512
 
 #endif /* HOOK_CONSTANTS_H */
