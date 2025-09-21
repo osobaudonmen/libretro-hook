d3250ff9eb4a50aa151f9ef78b071ca73bbf50b2
yuichietsu <osoba.udon.men@gmail.com>
2025-09-21 16:42:36 +0900

    Update hook_globals.h and libretro.c

--- main/src/hook_globals.h
+++ main/src/hook_globals.h
@@ -10,5 +10,6 @@ extern struct retro_log_callback logging;
 extern retro_log_printf_t log_cb;
 extern retro_environment_t environ_cb;
 extern retro_video_refresh_t video_cb;
+extern char retro_display_messages[4096];
 
 #endif /* HOOK_GLOBALS_H */
